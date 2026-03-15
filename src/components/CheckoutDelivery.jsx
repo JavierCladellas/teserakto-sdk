b56d6c486def8a837e10ed5e40d4494c0f96f2b5
JavierCladellas <javiercladellas@gmail.com>
2026-03-15 08:24:58 +0100

    add names

--- src/components/CheckoutDelivery.jsx
+++ src/components/CheckoutDelivery.jsx
@@ -2,22 +2,23 @@ import { useState } from "react";
 import { TextField } from "./TextField";
 import SearchableDropdown from "./SearchableDropdown";
 import DateField from "./DatePicker";
+import countryList from "react-select-country-list";
 
 
 const CheckoutDelivery = ({ settings, deviceSettings }) => {
     const [deliveryMethod, setDeliveryMethod] = useState("delivery");
+    const [deliveryCountry, setDeliveryCountry] = useState("");
 
-    //Today
     const [date, setDate] = useState(new Date());
     const [timeSlot, setTimeSlot] = useState("morning");
     
     return (
         <section className="mb-8">
-            <h2
-                className="font-semibold mb-4 text-lg"
-                style={{ fontSize: deviceSettings.titleFontSize }}
-            >{settings.deliveryHeading}</h2>
+            <h2 className="font-semibold mb-4 text-lg" style={{ fontSize: deviceSettings.titleFontSize }}>
+                {settings.deliveryHeading}
+            </h2>
             <div className="mb-4 flex gap-4">
+                <input type="hidden" name="delivery_type" value={deliveryMethod} />
                 {settings.allowPickups === true && (
                     <>
                         <button
@@ -47,6 +48,7 @@ const CheckoutDelivery = ({ settings, deviceSettings }) => {
                 <>
                 <div className="grid grid-cols-1 gap-4 mb-4">
                     <TextField
+                        name="delivery_address"
                         label={settings.deliveryAddressLabel }
                         placeholder={settings.deliveryAddressPlaceholder }
                         style={{ fontSize: deviceSettings.formFieldFontSize }}
@@ -54,27 +56,32 @@ const CheckoutDelivery = ({ settings, deviceSettings }) => {
                 </div>
                 <div className={`grid gap-4 mb-4 'grid-cols-1'md:grid-cols-1 sm:grid-cols-3`}> 
                     <TextField
+                        name="delivery_city"
                         label={settings.deliveryCityLabel }
                         placeholder={settings.deliveryCityPlaceholder }
                         style={{ fontSize: deviceSettings.formFieldFontSize }}
                     />
                     <TextField
+                        name="delivery_postal_code"
                         label={settings.deliveryPostalCodeLabel }
                         placeholder={settings.deliveryPostalCodePlaceholder }
                         style={{ fontSize: deviceSettings.formFieldFontSize }}
                     />
-                    <TextField
+                    <SearchableDropdown
+                        name="delivery_country"
                         label={settings.deliveryCountryLabel }
                         placeholder={settings.deliveryCountryPlaceholder }
+                        options={countryList().getData()}
                         style={{ fontSize: deviceSettings.formFieldFontSize }}
+                        value={deliveryCountry}
+                        onChange={e => setDeliveryCountry(e.target.value)}
                     />
                 </div>
                 {settings.allowDeliveryDate !== false && (
                     <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-4">
-                        <DateField value={date} onChange={setDate} label={settings.deliveryDeliveryDateLabel} placeholder="DD-MM-YYYY" style={{ fontSize: deviceSettings.formFieldFontSize }} />
+                        <DateField name="prefered_delivery_date" value={date} onChange={setDate} label={settings.deliveryDeliveryDateLabel} placeholder="DD-MM-YYYY" style={{ fontSize: deviceSettings.formFieldFontSize }} />
                         
-                        {/* BUG THIS IS NOT WORKING */}
-                        <SearchableDropdown label={settings.deliveryTimeSlotLabel}  value={timeSlot} onChange={e => setTimeSlot(e.target.value)} options={[
+                        <SearchableDropdown name="prefered_delivery_time_slot" label={settings.deliveryTimeSlotLabel}  value={timeSlot} onChange={e => setTimeSlot(e.target.value)} options={[
                             { label: "Morning (9AM-12PM)", value: "morning" },
                             { label: "Afternoon (12PM-5PM)", value: "afternoon" },
                             { label: "Evening (5PM-8PM)", value: "evening" },
@@ -85,6 +92,7 @@ const CheckoutDelivery = ({ settings, deviceSettings }) => {
             )}
             <div className="grid grid-cols-1 gap-4">
                 <TextField
+                    name="delivery_instructions" 
                     label={settings.deliveryNotesLabel }
                     placeholder={settings.deliveryNotesPlaceholder}
                     type="textarea"
--- src/components/CheckoutGifts.jsx
+++ src/components/CheckoutGifts.jsx
@@ -4,9 +4,11 @@ const CheckoutGifts = ({ deviceSettings }) => {
 
     return (
         <section className="mb-8">
+            <input type="hidden" name="is_alt_recipient" value="true" />
             <h2 className="font-semibold mb-4 text-lg" style={{ fontSize: deviceSettings.titleFontSize }}>{deviceSettings.giftInfoHeading}</h2>
             <div className="grid grid-cols-1 gap-4">
                 <TextField
+                    name="alt_recipient_name"
                     label={deviceSettings.giftRecipientNameLabel }
                     placeholder={deviceSettings.giftRecipientNamePlaceholder}
                     style={{ fontSize: deviceSettings.formFieldFontSize }}
@@ -14,11 +16,13 @@ const CheckoutGifts = ({ deviceSettings }) => {
             </div>
             <div className={`grid gap-4 mt-4 grid-cols-1 md:grid-cols-1 sm:grid-cols-2`}> 
                 <TextField
+                    name="alt_recipient_email"
                     label={deviceSettings.giftRecipientEmailLabel }
                     placeholder={deviceSettings.giftRecipientEmailPlaceholder }
                     style={{ fontSize: deviceSettings.formFieldFontSize }}
                 />
                 <TextField
+                    name="alt_recipient_phone"
                     label={deviceSettings.giftRecipientPhoneLabel }
                     placeholder={deviceSettings.giftRecipientPhonePlaceholder}
                     style={{ fontSize: deviceSettings.formFieldFontSize }}
@@ -26,6 +30,7 @@ const CheckoutGifts = ({ deviceSettings }) => {
             </div>
             <div className="grid grid-cols-1 gap-4 mt-4">
                 <TextField
+                    name="personal_message"
                     label={deviceSettings.giftPersonalMessageLabel }
                     placeholder={deviceSettings.giftPersonalMessagePlaceholder }
                     type="textarea"
