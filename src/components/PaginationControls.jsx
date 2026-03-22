51e690033925216cfa38f5ef769b88cc2bee9709
JavierCladellas <javiercladellas@gmail.com>
2026-03-22 08:23:10 +0100

    fix weird chevron

--- src/components/PaginationControls.jsx
+++ src/components/PaginationControls.jsx
@@ -3,7 +3,7 @@ import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 const PaginationControls = ({ currentPage, totalPages, setCurrentPage }) => {
     return (
         <div className="flex justify-end items-center gap-2 mt-4 px-4 py-2 text-sm">
-            <button className={`px-3 py-1 transition select-none flex items-center justify-center w-10 h-10 rounded-full bg-white border border-gray-300 text-gray-600 hover:bg-indigo-50 hover:text-indigo-600 shadow-sm disabled:opacity-50 disabled:cursor-not-allowed flex items-center justify-center`}
+            <button className={`px-3 py-1 transition select-none flex items-center justify-center w-10 h-10 rounded-full bg-white border border-solid border-gray-300 text-gray-600 hover:bg-indigo-50 hover:text-indigo-600 shadow-sm disabled:opacity-50 disabled:cursor-not-allowed flex items-center justify-center`}
                 onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
                 disabled={currentPage === 1}
                 type="button"
@@ -15,12 +15,12 @@ const PaginationControls = ({ currentPage, totalPages, setCurrentPage }) => {
                 Page <span className="font-medium">{currentPage}</span> of <span className="font-medium">{totalPages}</span>
             </span>
 
-            <button className={`px-3 py-1 transition select-none flex items-center justify-center w-10 h-10 rounded-full bg-white border border-gray-300 text-gray-600 hover:bg-indigo-50 hover:text-indigo-600 shadow-sm disabled:opacity-50 disabled:cursor-not-allowed flex items-center justify-center`}
+            <button className={`px-3 py-1 transition select-none flex items-center justify-center w-10 h-10 rounded-full bg-white border border-solid border-gray-300 text-gray-600 hover:bg-indigo-50 hover:text-indigo-600 shadow-sm disabled:opacity-50 disabled:cursor-not-allowed flex items-center justify-center`}
                 onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))}
                 disabled={currentPage === totalPages}
                 type="button"
             >
-                <FaChevronRight/>
+                <span className="flex items-center justify-center">{<FaChevronRight/>}</span>
             </button>
         </div>
     );
