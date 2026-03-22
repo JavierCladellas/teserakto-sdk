7485c47fa310fcfdceceabddd6ad517001a10d19
JavierCladellas <javiercladellas@gmail.com>
2026-03-22 06:29:27 +0100

    try fixing this tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/sdk-entry.jsx'),
-      name: 'TeseraktoShopSDK',
-      fileName: (format) => `shop-sdk.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'], // don’t bundle react
-    },
+// tailwind.config.js
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
   },
-});
+  plugins: [],
+};
